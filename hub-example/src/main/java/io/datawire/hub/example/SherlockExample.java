6d22bb98b62d1b35c64c67501d4b0b964c6ceb7b
Philip Lombardi <plombardi@datawire.io>
2015-12-02 16:14:25 -0500

    Example code commit

--- hub-example/src/main/java/io/datawire/hub/example/SherlockExample.java
+++ hub-example/src/main/java/io/datawire/hub/example/SherlockExample.java
@@ -2,21 +2,50 @@ package io.datawire.hub.example;
 
 
 import hub.*;
-import io.datawire.quark.runtime.JSONObject;
+import hub.event.RegistryUpdate;
+import io.datawire.quark.runtime.Runtime;
 
 public class SherlockExample {
+
+  static class Sherlock extends RegistryClient implements Runnable {
+
+    Sherlock(Runtime runtime) {
+      super(runtime, "localhost", 1234);
+    }
+
+    @Override public void run() {
+      onExecute(runtime);
+    }
+
+    @Override public void onExecute(io.datawire.quark.runtime.Runtime runtime) {
+      System.out.println("beep boop");
+      runtime.schedule(this, 2.0);
+    }
+  }
 
   public static void main(String... args) {
     io.datawire.quark.runtime.Runtime runtime = new io.datawire.quark.netty.QuarkNettyRuntime();
 
-    String hubAddress = "ws://localhost:1234";
-    final Sherlock sherlock = new Sherlock(runtime, hubAddress, new HubMessageProcessor());
-    sherlock.connect();
+    Sherlock sherlock = new Sherlock(runtime);
+    sherlock.run();
+
+//    RegistrySubscription rs = new RegistrySubscription(new SherlockHandler(), "localhost", 1234);
+//    rs.subscribe("localhost", 1234);
+
+//    String hubAddress = "ws://localhost:1234";
+//    final Sherlock sherlock = new Sherlock(runtime, hubAddress, new HubMessageProcessor());
+//    sherlock.connect();
   }
 
-  public static class HubMessageProcessor implements OnMessage {
-    @Override public void run(JSONObject json) {
-      System.out.println(json.toString());
+  public static class SherlockHandler extends DefaultRegistryHandler {
+    @Override public void onRegistryUpdate(RegistryUpdate update) {
+
     }
   }
+
+//  public static class HubMessageProcessor implements OnMessage {
+//    @Override public void run(JSONObject json) {
+//      System.out.println(json.toString());
+//    }
+//  }
 }
--- hub-example/src/main/java/io/datawire/hub/example/WatsonExample.java
+++ hub-example/src/main/java/io/datawire/hub/example/WatsonExample.java
@@ -1,7 +1,8 @@
 package io.datawire.hub.example;
 
 
-import hub.*;
+import hub.event.RegistryUpdate;
+import hub.model.HealthCheck;
 import io.datawire.quark.runtime.Runtime;
 
 import java.net.HttpURLConnection;
@@ -15,17 +16,39 @@ public class  WatsonExample {
 
     int port = Integer.valueOf(args[0]);
 
-    String hubAddress = "localhost:1234";
-    String serviceName = "foobar-service";
-    ServiceEndpoint endpoint = new ServiceEndpoint(
-        new NetworkAddress("127.0.0.1", "ipv4"),
-        new ServicePort("http", port)
-    );
+    String hubAddress = "localhost";
+    int hubPort = 1234;
 
-    Watson watson = new Watson(runtime, "ws://" + hubAddress, serviceName, endpoint);
-    watson.registerHealthCheck(new BogusHealthCheck(port));
-    watson.connect();
-    watson.register();
+    HealthCheck check = () -> {
+      try {
+        final URL helloService = URI.create("http://127.0.0.1:" + port).toURL();
+        final HttpURLConnection conn = (HttpURLConnection) helloService.openConnection();
+        return conn.getResponseCode() / 100 == 2;
+      } catch (Exception any) {
+        return false;
+      }
+    };
+
+
+
+
+
+//
+//    String hubAddress = "localhost:1234";
+//    String serviceName = "foobar-service";
+//    ServiceEndpoint endpoint = new ServiceEndpoint(
+//        new NetworkAddress("127.0.0.1", "ipv4"),
+//        new ServicePort("http", port)
+//    );
+
+    RegistryUpdate update = new RegistryUpdate(null, null);
+    System.out.println(update.records.size());
+
+
+//    Watson watson = new Watson(runtime, "ws://" + hubAddress, serviceName, endpoint);
+//    watson.registerHealthCheck(new BogusHealthCheck(port));
+//    watson.connect();
+//    watson.register();
   }
 
   public static class BogusHealthCheck implements HealthCheck {
