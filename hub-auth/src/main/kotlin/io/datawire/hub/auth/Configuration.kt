6848dc6bd0ff92c4b7051112e940ce28f518eceb
Philip Lombardi <plombardi@datawire.io>
2015-12-31 14:54:43 -0500

    hub-auth is now hub-gateway

--- hub-auth/src/main/kotlin/io/datawire/hub/auth/Configuration.kt
+++
@@ -1,7 +0,0 @@
-package io.datawire.hub.auth
-
-import com.fasterxml.jackson.annotation.JsonProperty
-import io.datawire.hub.auth.jwt.JwtProviderFactory
-
-
-data class Configuration(@JsonProperty("jwt") val jwtFactory: JwtProviderFactory)
