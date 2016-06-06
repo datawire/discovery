e3e989636882f9e4db246f71d4e9b08d9b4f4fee
Philip Lombardi <plombardi@datawire.io>
2016-06-06 17:31:22 -0400

    Add some debug logging to the servicesstore listener class so we can see when things are being added, removed or evicted during debug

--- discovery-web/src/main/kotlin/io/datawire/discovery/service/ServicesChangeListener.kt
+++ discovery-web/src/main/kotlin/io/datawire/discovery/service/ServicesChangeListener.kt
@@ -5,25 +5,34 @@ import com.hazelcast.core.EntryListener
 import com.hazelcast.core.MapEvent
 import io.datawire.discovery.model.ServiceRecord
 import io.vertx.core.eventbus.EventBus
+import io.vertx.core.logging.LoggerFactory
 
 
 class ServicesChangeListener(private val eventBus: EventBus) : EntryListener<String, ServiceRecord> {
 
+  private val logger = LoggerFactory.getLogger(javaClass)
+
   override fun entryAdded(event: EntryEvent<String, ServiceRecord>?) {
     val record  = event!!.value
     val message = record.toActive()
+
+    logger.debug("Entry added (key: {})", record.key)
     eventBus.publish("datawire.discovery.${record.key.tenant}.services.notifications", message.encode())
   }
 
   override fun entryEvicted(event: EntryEvent<String, ServiceRecord>?) {
     val record  = event!!.oldValue
     val message = record.toExpire()
+
+    logger.debug("Entry evicted (key: {})", record.key)
     eventBus.publish("datawire.discovery.${record.key.tenant}.services.notifications", message.encode())
   }
 
   override fun entryRemoved(event: EntryEvent<String, ServiceRecord>?) {
     val record  = event!!.value
     val message = record.toExpire()
+
+    logger.debug("Entry removed (key: {})", record.key)
     eventBus.publish("datawire.discovery.${record.key.tenant}.services.notifications", message.encode())
   }
 
