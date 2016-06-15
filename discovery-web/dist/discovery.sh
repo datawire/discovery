5141e5719de3f258f47ad59ff5e6fec71b97de25
Philip Lombardi <plombardi@datawire.io>
2016-06-15 15:28:34 -0400

    Remove log files that are in the log directory before starting the server

--- discovery-web/dist/discovery.sh
+++ discovery-web/dist/discovery.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rm -f /var/log/datawire/*.log
+
 java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
      -Dhazelcast.logging.type=slf4j \
      -jar /opt/discovery/discovery-web.jar \
