99837cdd874575400e2ca745bab9ac7051a2a588
Philip Lombardi <plombardi@datawire.io>
2016-06-29 14:06:18 -0400

    Update launch script

--- discovery-web/dist/discovery.sh
+++ discovery-web/dist/discovery.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 rm -f /var/log/datawire/*.log
 
 java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
-     -Dhazelcast.logging.type=slf4j \
+     -Dvertx.hazelcast.config=/etc/discovery/cluster.xml \
      -jar /opt/discovery/discovery-web.jar \
      -conf /etc/discovery/discovery.json \
-     -cluster /etc/discovery/cluster.xml
+     -cluster
